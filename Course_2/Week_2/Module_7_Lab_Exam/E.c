c8996161928f367ccf05a8b462c650c680f29809
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-12 03:23:50 +0600

    assignment 7E done

--- Course_2/Week_2/Module_7_Lab_Exam/E.c
+++ Course_2/Week_2/Module_7_Lab_Exam/E.c
@@ -6,17 +6,17 @@ int main()
     scanf("%d %d %d", &X, &Y, &N);
     for (int i = 1; i <= N; i++)
     {
-        if (i % X == 0)
+        if (i % X == 0 && i % Y == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % X == 0)
         {
             printf("Fizz\n");
         }
         else if (i % Y == 0)
         {
             printf("Buzz\n");
-        }
-        else if (i % X == 0 && i % Y == 0)
-        {
-            printf("FizzBuzz\n");
         }
         else
         {
