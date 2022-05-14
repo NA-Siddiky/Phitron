a33476dbaa4140934c3f306aef59e24b3f92be29
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-15 01:51:09 +0600

    problem F

--- Course_2/Week_2/Module_7_Lab_Exam/F.c
+++ Course_2/Week_2/Module_7_Lab_Exam/F.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
-
-    printf("Hello, world");
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    if ((A == 5 || A == 7) && (B == 5 || B == 7) && (C == 5 || C == 7))
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
 }
