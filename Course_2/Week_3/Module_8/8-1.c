27b3b12939fece9d1f34abfcd377d2cf89cff866
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-16 16:49:52 +0600

    8.1 done

--- Course_2/Week_3/Module_8/8-1.c
+++ Course_2/Week_3/Module_8/8-1.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1;
-    while (i <= 5)
+    // int i = 1;
+    // while (i <= 5)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+    int money = 0;
+    int target = 100;
+    while (money < target)
     {
-        printf("%d\n", i);
-        i++;
+        printf("need money. Enter amount: ");
+        int given;
+        scanf("%d", &given);
+        money += given;
+        printf("Current money: %d\n", money);
     }
-
-    return 0;
 }
