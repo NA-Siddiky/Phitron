56806d9746d41d0ee003024b7457e47815c9a5d8
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-18 01:20:17 +0600

    module 9-4

--- Course_2/Week_3/Module_9/9-4.c
+++ Course_2/Week_3/Module_9/9-4.c
@@ -1,3 +1,27 @@
+// #include <stdio.h>
+// int main()
+// {
+//     char ch;
+//     scanf("%c", &ch);
+
+//     if ('A' <= ch && ch <= 'Z')
+//     {
+//         printf("Uppercase");
+//     }
+//     else if ('a' <= ch && ch <= 'z')
+//     {
+//         printf("Lowercase");
+//     }
+//     else if ('0' <= ch && ch <= '9')
+//     {
+//         printf("Number");
+//     }
+//     else
+//     {
+//         printf("Others character");
+//     }
+// }
+
 #include <stdio.h>
 int main()
 {
@@ -6,18 +30,11 @@ int main()
 
     if ('A' <= ch && ch <= 'Z')
     {
-        printf("Uppercase");
-    }
-    else if ('a' <= ch && ch <= 'z')
-    {
-        printf("Lowercase");
-    }
-    else if ('0' <= ch && ch <= '9')
-    {
-        printf("Number");
+        char lowercase = ch - 'A' + 'a';
+        printf("Lowercase =%c", lowercase);
     }
     else
     {
-        printf("Others character");
+        printf("No Uppercase character");
     }
 }
