90f66fa4539977750df4b1cd197dd1e9bd0993ca
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-17 17:10:45 +0600

    8-4 done

--- Course_2/Week_3/Module_8/8-4.c
+++ Course_2/Week_3/Module_8/8-4.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
-    for (int i = 1; i <= 10; i++)
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\n", i);
+    //     if (i == 5)
+    //         break;
+    // }
+
+    while (true)
     {
-        printf("%d\n", i);
-        if (i == 5)
+        int input;
+        scanf("%d", &input);
+        printf("%d\n", input);
+
+        if (input != 0 && input != 1)
+        {
+            printf("Invalid input\n");
             break;
+        }
     }
 }
