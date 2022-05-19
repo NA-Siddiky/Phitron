ca7d6b5f3a33833019e324166c5e395cecef5de8
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-19 15:50:21 +0600

    module 9-6

--- Course_2/Week_3/Module_9/9-6.c
+++ Course_2/Week_3/Module_9/9-6.c
@@ -1,23 +1,12 @@
-// #include <stdio.h>
-// int main()
-// {
-//     for (int i = 0; i <= 10; i++)
-//     {
-//         if (i == 5)
-//             break;
-//         {
-//             printf("%d\n", i);
-//         }
-//     }
-// }
-
 #include <stdio.h>
 int main()
 {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    A = 2 * A + 100 - B;
-    printf("%d\n", A);
-
-    return 0;
+    for (int i = 0; i <= 10; i++)
+    {
+        if (i == 5)
+            break;
+        {
+            printf("%d\n", i);
+        }
+    }
 }
