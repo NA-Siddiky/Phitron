a42cd77b5b26dd25dded4e2fd92d34b5f2d54d56
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-19 17:20:58 +0600

    module 9-5 B done

--- Course_2/Week_3/Module_9/9-5Problemset.c
+++ Course_2/Week_3/Module_9/9-5Problemset.c
@@ -10,26 +10,26 @@
 // // }
 
 #include <stdio.h>
-int main()
+#include <string.h>
+int main(void)
 {
-
-    int x = 0;
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    int t, n = 0;
+    char ch[4];
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
     {
-        char a, b, c;
-        scanf("%c%c%c", &a, &b, &c);
-        if (b == '+')
+        scanf("%s", &ch);
+        if ((strcmp(ch, "X++") == 0) || (strcmp(ch, "++X") == 0))
         {
-            x++;
+            n++;
         }
-        else if (b == '-')
+        else if ((strcmp(ch, "X--") == 0) || (strcmp(ch, "--X") == 0))
         {
-            x--;
+            n--;
         }
     }
-    printf("%d\n", x);
+
+    printf("%d\n", n);
 }
 
 // #include <stdio.h>
