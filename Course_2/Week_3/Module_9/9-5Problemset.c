35f3aeb84c69b6a10da90f718879ab984740bc47
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-19 16:53:23 +0600

    module 9-5

--- Course_2/Week_3/Module_9/9-5Problemset.c
+++ Course_2/Week_3/Module_9/9-5Problemset.c
@@ -1,10 +1,33 @@
+// // #include <stdio.h>
+// // int main()
+// // {
+// //     int A, B;
+// //     scanf("%d %d", &A, &B);
+// //     A = 2 * A + 100 - B;
+// //     printf("%d\n", A);
+
+// //     return 0;
+// // }
+
 #include <stdio.h>
 int main()
 {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    A = 2 * A + 100 - B;
-    printf("%d\n", A);
+    char a, b, c;
 
-    return 0;
+    int x = 0;
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%c%c%c", &a, &b, &c);
+        if (b == '+')
+        {
+            x++;
+        }
+        else if (b == '-')
+        {
+            x--;
+        }
+    }
+    printf("%d\n", x);
 }
