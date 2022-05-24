8207e6c2924e140892c4d010ecb4ccdadbf5c5a5
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-24 18:46:10 +0600

    11B

---
+++ Course_2/Week_3/Module_11_ExamDay/B.c
@@ -0,0 +1,38 @@
+/*
+room = n;
+room num: i;
+num of people: p;
+room accommodation: q;
+
+p<q;
+
+
+input:
+n;
+p, q;
+
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, p, q;
+    scanf("%d", &n);
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &p, &q);
+        if (p < q)
+        {
+            count++;
+        }
+        else
+        {
+        }
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
