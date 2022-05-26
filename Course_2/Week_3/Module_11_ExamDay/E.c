4047978ba4ff03afd669a1090c351313b1be50e1
NA-Siddiky <siddiky.academic@gmail.com>
2022-05-26 15:15:50 +0600

    11E

--- Course_2/Week_3/Module_11_ExamDay/E.c
+++ Course_2/Week_3/Module_11_ExamDay/E.c
@@ -27,36 +27,70 @@ int main(void)
 {
 
     int A, B, C, D, E, F, X;
-    A = 4;
-    B = 3;
-    C = 3;
-    D = 6;
-    E = 2;
-    F = 5;
-    X = 10;
+    // A = 4;
+    // B = 3;
+    // C = 3;
+    // D = 6;
+    // E = 2;
+    // F = 5;
+    // X = 10;
+    scanf("%d %d %d %d %d %d %d", &A, &B, &C, &D, &E, &F, &X);
 
     int a = 0;
     int b = 0;
 
+    // for (int i = 0; i <= X; i++)
+    // {
+    //     if (A < X)
+    //     {
+    //         a = (X - (A + C));
+    //     }
+
+    //     result1 = (A + a) * B;
+
+    // }
+    // printf("%d\n", result1);
+
     for (int i = 0; i <= X; i++)
     {
-        if (X > (A * B))
+
+        if (A < X)
         {
-            a = (X - (A + C)) * B;
+            if (X > (A + C))
+            {
+                a = (A + (X - (A + C))) * B;
+            }
+            else
+            {
+                a = A * B;
+            }
+        }
+
+        if (D < X)
+        {
+            if (X > (D + F))
+            {
+                b = (D + (X - (D + F))) * E;
+            }
+            else
+            {
+                b = D * E;
+            }
         }
     }
-    printf("%d\n", a);
+    // printf("%d\n", a);
+    // printf("%d\n", b);
 
-    // if (a > b)
-    // {
-    //     printf("Takahashi\n");
-    // }
-    // if (b > a)
-    // {
-    //     printf("Aoki\n");
-    // }
-    // if (a == b)
-    // {
-    //     printf("Draw\n");
-    // }
+    if (a > b)
+    {
+        printf("Takahashi\n");
+    }
+    if (b > a)
+    {
+        printf("Aoki\n");
+    }
+    if (a == b)
+    {
+        printf("Draw\n");
+    }
 }
