69d4111705b62367b341cf7c4c93148a3a531263
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-03 01:31:54 +0600

    11F

--- Course_2/Week_3/Module_11_ExamDay/F.c
+++ Course_2/Week_3/Module_11_ExamDay/F.c
@@ -2,23 +2,25 @@
 
 3<=n, m<50
 n is odd number
-drives on n by m board
-
-
-
+drives on n by m
 */
 
 #include <stdio.h>
 int main()
 {
-    int n = 3;
+    int n = 9;
     int m = 4;
 
-    int c = '#';
+    int hash = '#';
+    int dot = '.';
 
     for (int i = 0; i < n; i++)
     {
-        printf("%c\n", c);
+        printf("%c\n", hash);
+        for (int j = 0; j < m; j++)
+        {
+            printf("%c", dot);
+        }
     }
 
     return 0;
