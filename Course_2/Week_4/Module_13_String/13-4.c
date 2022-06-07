52fdae9207610ed9faa09f9e2dd5bb038bdf6f46
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-07 13:17:40 +0600

    M13-4 done

--- Course_2/Week_4/Module_13_String/13-4.c
+++ Course_2/Week_4/Module_13_String/13-4.c
@@ -6,9 +6,17 @@ int main()
     char str2[10];
 
     int len = strlen(str1);
-    for (int i = 0; i < len; i++)
+    // for (int i = 0; i < len; i++)
+    // for (int i = 0; str1[i] != 0; i++)
+    // {
+    //     str2[i] = str1[i];
+    // }
+
+    int i = 0;
+    while (str1[i] != '\0')
     {
         str2[i] = str1[i];
+        i++;
     }
     str2[len] = '\0';
     printf("new: %s\n", str2);
