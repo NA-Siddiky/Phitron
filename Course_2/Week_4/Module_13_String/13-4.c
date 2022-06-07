2886e5b5b9a4bb7ef7958f95a14573012e44b20f
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-07 13:26:21 +0600

    M13-4 done

--- Course_2/Week_4/Module_13_String/13-4.c
+++ Course_2/Week_4/Module_13_String/13-4.c
@@ -2,15 +2,19 @@
 #include <string.h>
 int main()
 {
-    char str1[] = "hello123 check";
+    char str1[] = "hello123 check ius ok";
     char str2[10];
 
+    // strcpy(str2, str1);
+    // strncpy(str2, str1, 15);
+    // str2[5] = '\0';
+
     int len = strlen(str1);
-    // for (int i = 0; i < len; i++)
-    // for (int i = 0; str1[i] != 0; i++)
-    // {
-    //     str2[i] = str1[i];
-    // }
+    for (int i = 0; i < len; i++)
+        for (int i = 0; str1[i] != 0; i++)
+        {
+            str2[i] = str1[i];
+        }
 
     int i = 0;
     while (str1[i] != '\0')
@@ -19,5 +23,6 @@ int main()
         i++;
     }
     str2[len] = '\0';
+
     printf("new: %s\n", str2);
 }
