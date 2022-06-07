18262f155e61c3b523b2006f600cc176565cde51
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-07 11:22:03 +0600

    M13-2

--- Course_2/Week_4/Module_13_String/13-2.c
+++ Course_2/Week_4/Module_13_String/13-2.c
@@ -4,8 +4,11 @@ int main()
     printf("Enter your Name: ");
 
     char name[100];
-    scanf("%s", name);
-    printf("Hello, %s\n");
+    // scanf("%s", name);
+    // printf("Hello, %s\n", name);
+
+    gets(name);
+    printf("Hello, %s\n", name);
 
     return 0;
 }
