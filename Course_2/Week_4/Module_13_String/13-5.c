87fe72247d9bac139cd95c058209c73740a57f2e
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-07 17:19:22 +0600

    M13-5 done

--- Course_2/Week_4/Module_13_String/13-5.c
+++ Course_2/Week_4/Module_13_String/13-5.c
@@ -2,32 +2,42 @@
 #include <string.h>
 int main()
 {
-    char a[] = "Hello";
-    char b[] = "World";
+    char a[100] = "Hello";
+    char b[100] = "World";
+
+    int lena = strlen(a);
+    int lenb = strlen(b);
+
+    for (int i = 0; i < lenb; i++)
+    {
+        a[lena + i] = b[i];
+    }
+    a[lena + lenb] = '\0';
+    printf("%s\n", a);
 
     // strcpy(c, a);
     // strcat(c, " ");
     // strcat(c, b);
 
-    int lena = strlen(a);
-    int lenb = strlen(b);
-    int lenc = lena + lenb;
+    // int lena = strlen(a);
+    // int lenb = strlen(b);
+    // int lenc = lena + lenb;
 
-    int i;
-    char c[lena + 1];
+    // int i;
+    // char c[lena + 1];
 
-    for (i = 0; i < lena; i++)
-    {
-        c[i] = a[i];
-    }
-    c[i] = ' ';
+    // for (i = 0; i < lena; i++)
+    // {
+    //     c[i] = a[i];
+    // }
+    // c[i] = ' ';
 
-    int j, k;
-    for (j = 0, k = i + 1; j < lenb; j++, k++)
-    {
-        c[k] = b[j];
-    }
+    // int j, k;
+    // for (j = 0, k = i + 1; j < lenb; j++, k++)
+    // {
+    //     c[k] = b[j];
+    // }
 
-    c[k] = '\0';
-    printf("%s\n", c);
+    // c[k] = '\0';
+    // printf("%s\n", c);
 }
