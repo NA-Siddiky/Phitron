21e1259f8e7b2a09ba73d41840ce67e6f90a26bc
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-08 12:57:51 +0600

    13-5-2 max array

--- Course_2/Week_4/Module_13.5_PracticeDay1/13-5-1_maxArr.c
+++ Course_2/Week_4/Module_13.5_PracticeDay1/13-5-1_maxArr.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 int main()
 {
-    int a[] = {10, 20, 50, 40, 50, 30, 40, 60};
+    int a[] = {10, 20, 50, 40, 50, 30, 40};
+    int result[100] = {0};
 
-    int sizeOfArr = sizeof(a) / sizeof(a[0]);
+    int size = sizeof(a) / sizeof(a[0]);
 
-    int Maximum, MaximumOccurs;
-    int x, y;
-    x = y = 0;
-    for (int i = 0; i < sizeOfArr; i++)
+    int count = 0;
+    for (int i = 1; i < size; ++i)
     {
-        int x = a[i];
-        int y = a[i + 1] > a[i] ? a[i] : a[i + 1];
-        printf("Result-1: %d\n", x);
-        printf("Result-2: %d\n", y);
-        if (x > y)
+        if (result[0] < a[i])
         {
-            // printf("Result-x: %d\n", x);
-            Maximum = x;
-            continue;
-        }
-        else if (y > x)
-        {
-            // printf("Result-y: %d\n", y);
-            Maximum = y;
-            continue;
+            result[0] = a[i];
+            count++;
         }
     }
-    printf("Maximum: %d\n", Maximum);
+    printf("Largest element = %d", result[0]);
+    printf("Count element = %d", count);
+    return 0;
+
+    // int Maximum, MaximumOccurs;
+    // int x, y;
+    // x = y = 0;
+    // for (int i = 0; i < sizeOfArr; i++)
+    // {
+    //     int x = a[i];
+    //     int y = a[i + 1] > a[i] ? a[i] : a[i + 1];
+    //     // printf("Result-1: %d\n", x);
+    //     // printf("Result-2: %d\n", y);
+    //     if (x > y)
+    //     {
+    //         printf("Result-x: %d\n", x);
+    //         Maximum = x;
+    //         continue;
+    //     }
+    //     else if (y > x)
+    //     {
+    //         printf("Result-y: %d\n", y);
+    //         Maximum = y;
+    //         continue;
+    //     }
+    // }
+    // printf("Maximum: %d\n", Maximum);
     // printf("MaximumOccurs: %d\n", MaximumOccurs);
 }
