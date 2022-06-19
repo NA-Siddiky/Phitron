8991c3377f9eec5e1b167900a4464ac0dc3ac2a6
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-20 01:43:57 +0600

    P15-6

--- Course_2/Week_5_FunctionPointers/Module_16_Function/16-5_TheMinFunction.c
+++ Course_2/Week_5_FunctionPointers/Module_16_Function/16-5_TheMinFunction.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+
+int getMin(int a, int b)
+{
+    int temp;
+    if (a < b)
+    {
+        temp = a;
+    }
+    else
+    {
+        temp = b;
+    }
+    return temp;
+}
+
 int main()
 {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int temp = getMin(a, b);
+    int temp2 = getMin(temp, c);
+
+    printf("Min value %d\n", temp2);
 }
