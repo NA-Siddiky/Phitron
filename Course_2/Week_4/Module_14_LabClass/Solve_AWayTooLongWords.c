0e447d88e63efb05710b75a7065e0ae03e105275
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-19 13:11:11 +0600

    P14-5 solved & submit

--- Course_2/Week_4/Module_14_LabClass/Solve_AWayTooLongWords.c
+++ Course_2/Week_4/Module_14_LabClass/Solve_AWayTooLongWords.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 int main()
 {
-    int t;
-    scanf("%d", &t);
+    int n;
+    scanf("%d", &n);
 
-    for (int i = 0; i < t; i++)
+    for (int i = 0; i < n; i++)
     {
         char str[101];
         scanf("%s", str);
+
         int len = strlen(str);
-        if (len < 10)
+        if (len <= 10)
         {
             printf("%s\n", str);
         }
@@ -20,6 +20,4 @@ int main()
             printf("%c%d%c\n", str[0], len - 2, str[len - 1]);
         }
     }
-
-    return 0;
 }
