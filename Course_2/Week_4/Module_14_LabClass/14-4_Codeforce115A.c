90316d17320dae60469862582f77640dce32e46b
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-19 12:06:09 +0600

    P14-4

--- Course_2/Week_4/Module_14_LabClass/14-4_Codeforce115A.c
+++ Course_2/Week_4/Module_14_LabClass/14-4_Codeforce115A.c
@@ -2,13 +2,17 @@
 int main()
 {
     int n;
-    scanf("%d", n);
+    scanf("%d", &n);
     int score[n];
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", score[i]);
+        scanf("%d", &score[i]);
+        printf("%d ", score[i]);
     }
-    printf("%d", score[n]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", score[i]);
+    }
 
     return 0;
 }
