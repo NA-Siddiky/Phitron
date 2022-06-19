23738da01e00a40113624caa9c453310aa6a366f
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-20 01:53:20 +0600

    P15-6 done

--- Course_2/Week_5_FunctionPointers/Module_16_Function/16-5_TheMinFunction.c
+++ Course_2/Week_5_FunctionPointers/Module_16_Function/16-5_TheMinFunction.c
@@ -16,11 +16,26 @@ int getMin(int a, int b)
 
 int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
 
-    int temp = getMin(a, b);
-    int temp2 = getMin(temp, c);
+    // int temp = getMin(a, b);
+    // int temp2 = getMin(temp, c);
 
-    printf("Min value %d\n", temp2);
+    // printf("Min value %d\n", temp2);
+
+    int n;
+    scanf("%d\n", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int ans = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        ans = getMin(ans, a[i]);
+    }
+    printf("%d", ans);
 }
