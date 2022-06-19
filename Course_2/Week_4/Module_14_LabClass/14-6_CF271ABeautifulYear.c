42c2f662d74bc5ed243c964fd429cf4cee4fc306
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-19 16:07:24 +0600

    P14-6 solved

--- Course_2/Week_4/Module_14_LabClass/14-6_CF271ABeautifulYear.c
+++ Course_2/Week_4/Module_14_LabClass/14-6_CF271ABeautifulYear.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
+    int year;
+    scanf("%d", &year);
 
-    return 0;
+    for (int y = year + 1;; y++)
+    {
+        int currentYear = y;
+        int fr[10] = {0};
+        bool repeat = false;
+
+        while (currentYear > 0)
+        {
+            int d = currentYear % 10;
+            currentYear /= 10;
+            // printf("%d ",d);
+            fr[d]++;
+
+            if (fr[d] >= 2)
+            {
+                repeat = true;
+            }
+        }
+        if (!repeat)
+        {
+            printf("%d\n", y);
+            break;
+        }
+    }
 }
