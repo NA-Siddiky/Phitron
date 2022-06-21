ae45eb51fdf567c1f46f8c0ec018d02b59452a05
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-21 17:17:44 +0600

    P17-3

--- Course_2/Week_5_FunctionPointers/Module_17_Pointers/17-3_PointerArithmatic.c
+++ Course_2/Week_5_FunctionPointers/Module_17_Pointers/17-3_PointerArithmatic.c
@@ -3,6 +3,9 @@ int main()
 {
     char ch = 'a';
     char *pch = &ch;
-    printf("%c\n", ch);
-    printf("pch points to %lld which is %c", pch, *pch);
+    // printf("%c\n", ch);
+    printf("pch points to %lld which is %c\n", pch, *pch);
+
+    pch++;
+    printf("now address is : %lld\n", pch);
 }
