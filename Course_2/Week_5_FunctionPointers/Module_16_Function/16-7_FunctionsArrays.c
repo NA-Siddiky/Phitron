c1b47ffb7a4230e00716aa6ef7d7895f0f90643f
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-21 12:51:00 +0600

    P15-7 solves

--- Course_2/Week_5_FunctionPointers/Module_16_Function/16-7_FunctionsArrays.c
+++ Course_2/Week_5_FunctionPointers/Module_16_Function/16-7_FunctionsArrays.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
+int getMin(int a, int b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
 int getArrayMin(int n, int a[])
 {
-    printf("%d %d", n, a);
+    int min = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        min = getMin(a[i], min);
+    }
+    return min;
 }
 
 int main()
 {
-    int a[] = {4, 5, 6, 7, 8, 9};
+    int a[] = {4, 5, 6, 7, 2, 9};
 
     int sz = sizeof(a) / sizeof(a[0]);
-    printf("%d", sz);
+    // printf("%d", sz);
 
-    getArrayMin(sz, a);
-    // printf("%d", getArrayMin(a));
+    printf("%d\n", getArrayMin(sz, a));
 }
