980f7ab3f00c6d5ed7186f806cf40b99f567d1be
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-21 13:09:13 +0600

    P15-7 solved

--- Course_2/Week_5_FunctionPointers/Module_16_Function/16-7_FunctionsArrays.c
+++ Course_2/Week_5_FunctionPointers/Module_16_Function/16-7_FunctionsArrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int getMin(int a, int b)
 {
@@ -18,12 +19,39 @@ int getArrayMin(int n, int a[])
     return min;
 }
 
+void makeDouble(int n, int a[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i] *= 2;
+    }
+}
+void reverse(char str[])
+{
+    int n = strlen(str);
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+    {
+        int temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
 int main()
 {
-    int a[] = {4, 5, 6, 7, 2, 9};
+    // int a[] = {4, 5, 6, 7, 2, 9};
 
-    int sz = sizeof(a) / sizeof(a[0]);
-    // printf("%d", sz);
+    // int sz = sizeof(a) / sizeof(a[0]);
+    // // printf("%d", sz);
 
-    printf("%d\n", getArrayMin(sz, a));
+    // printf("%d\n", getArrayMin(sz, a));
+
+    // makeDouble(sz, a);
+    // for (int i = 0; i < sz; i++)
+    // {
+    //     printf("%d  ", a[i]);
+    // }
+
+    char str[] = "hello";
+    reverse(str);
+    printf("%s", str);
 }
