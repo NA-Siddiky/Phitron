5e8fe9251c5b3681751520870d9bc94cf837741d
NA-Siddiky <siddiky.academic@gmail.com>
2022-06-21 10:32:42 +0600

    P15-6

--- Course_2/Week_5_FunctionPointers/Module_16_Function/16-6_FunctionScopes.c
+++ Course_2/Week_5_FunctionPointers/Module_16_Function/16-6_FunctionScopes.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+
+int getMin(int a, int b)
+{
+    int temp;
+    if (a < b)
+        temp = a;
+    else
+        temp = b;
+    return temp;
+}
+
 int main()
 {
+    int a = 10, b = 12;
+    int mn = getMin(a, b);
 }
