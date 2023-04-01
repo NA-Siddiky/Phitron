d10cc99e095232eb2bde116edd14451f22ff5012
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-02 00:53:30 +0600

    adding deta type example

---
+++ Class/Week_01/Module_01/output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // int rahim, karim;
+    // scanf("%d", &rahim);
+    // printf("%d\n", rahim);
+
+    int a, b;
+    // scanf("%d%% %d%%", &a, &b);
+    // printf("%d%% %d%%", a, b);
+    scanf("%da %db", &a, &b);
+    printf("%da %db", a, b);
+    return 0;
+}
--- Class/Week_01/Module_01/variable.c
+++ Class/Week_01/Module_01/variable.c
@@ -5,7 +5,10 @@ int main()
     float karim = 100.52;
     char habib = 'z';
 
-    // printf("%d\n", "%f\n", "%c\n", rahim, karim, habib);
+    printf("%d\n"
+           "%f\n"
+           "%c\n",
+           rahim, karim, habib);
     printf("%0.2f", karim);
     return 0;
 }
