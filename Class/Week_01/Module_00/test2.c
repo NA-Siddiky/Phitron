e925dfefc0b5b51252ab1f2faa08f82892778210
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-01 16:05:53 +0600

    check from windows test case 2

--- Class/Week_01/Module_00/test2.c
+++ Class/Week_01/Module_00/test2.c
@@ -4,7 +4,7 @@ int main()
     char ch = 'a';
     char *pch = &ch;
     // printf("%c\n", ch);
-    printf("pch points to %lld which is %c\n", pch, *pch);
+    printf("pch points to>> %lld which is %c\n", pch, *pch);
     pch++;
     printf("now address is : %lld\n", pch);
 }
