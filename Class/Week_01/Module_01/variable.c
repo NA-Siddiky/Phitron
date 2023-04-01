05230bb5c4dc5f3f6d48b91f33901415a1c8349e
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-02 00:33:16 +0600

    adding deta type

---
+++ Class/Week_01/Module_01/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int rahim = 10;
+    float karim = 100.52;
+    char habib = 'z';
+
+    // printf("%d\n", "%f\n", "%c\n", rahim, karim, habib);
+    printf("%0.2f", karim);
+    return 0;
+}
