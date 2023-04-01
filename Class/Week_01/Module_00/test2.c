505d4061b91ad96d18c591c39c45ac2642a68737
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-01 15:57:59 +0600

    check mac 2nd program

---
+++ Class/Week_01/Module_00/test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char ch = 'a';
+    char *pch = &ch;
+    // printf("%c\n", ch);
+    printf("pch points to %lld which is %c\n", pch, *pch);
+    pch++;
+    printf("now address is : %lld\n", pch);
+}
