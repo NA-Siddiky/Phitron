22705df8b72b2d4387b2c6ac06c75e189cbb5336
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-07 02:38:59 +0600

    check code done

---
+++ Class/Week_01/Module_3.5_Practice2/practice2_Sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x, y;
+    int sum = 0;
+    scanf("%d %d", &x, &y);
+    sum = x + y;
+    printf("%d", sum);
+    return 0;
+}
---
+++ Class/Week_01/Module_3.5_Practice2/practice4_Variables.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a;
+    long long b;
+    float c;
+    char d;
+
+    scanf("%d", &a);
+    scanf("%lld", &b);
+    scanf("%f", &c);
+    scanf(" %c", &d);
+
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%.2f\n", c);
+    printf("%c\n", d);
+
+    return 0;
+}
