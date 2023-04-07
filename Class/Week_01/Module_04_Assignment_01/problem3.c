f79bcf2059eb9f11a2f472daa77ba643158b4d6d
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-07 19:43:56 +0600

    solved problem 3

---
+++ Class/Week_01/Module_04_Assignment_01/problem3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 0 && n % 3 == 0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+    return 0;
+}
