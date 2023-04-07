d274d2d97a49849efad42eb70192bd96894f3f4f
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-07 20:03:28 +0600

    solved problem 4

--- Class/Week_01/Module_04_Assignment_01/problem4.c
+++ Class/Week_01/Module_04_Assignment_01/problem4.c
@@ -5,13 +5,15 @@
 
 int main()
 {
-
     int n;
     scanf("%d", &n);
-
-    if (n >= 0 && n % 3 == 0)
+    for (int i = 1; i <= n; i++)
     {
-        printf("YES\n");
+        if (i % 7 == 0 && i % 3 == 0)
+        {
+            printf("%d\n", i);
+        }
     }
+
     return 0;
 }
