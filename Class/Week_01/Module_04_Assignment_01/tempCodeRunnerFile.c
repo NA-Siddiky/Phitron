fb6e1c4e06d53e9261346db707ab64ab30467dec
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-07 19:35:01 +0600

    solved problem 2

--- Class/Week_01/Module_04_Assignment_01/tempCodeRunnerFile.c
+++ Class/Week_01/Module_04_Assignment_01/tempCodeRunnerFile.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main()
-{
-    int x, y;
-    scanf("%d %d\n", &x, &y);
-    long long multiply = (long long)x * y;
-    printf("%lld\n", multiply);
-    return 0;
-}
+(long long)
