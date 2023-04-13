e18145856d6e9395c45dd986d3d4bc046d21187a
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-13 15:14:21 +0600

    module 6 done

---
+++ Class/Week_02_Loop&Array/Module_06_Loop/nested_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        // kaj
+        for (int j = 1; j <= 5; j++)
+        {
+            printf("j=%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
