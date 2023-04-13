50dc68226a933f0a488d1f92ed79b63de419b768
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-13 14:52:12 +0600

    problem 5 solved

--- Class/Week_02_Loop&Array/Module_06_Loop/E_Max.c
+++ Class/Week_02_Loop&Array/Module_06_Loop/E_Max.c
@@ -6,7 +6,9 @@ int main()
     int n;
     scanf("%d", &n);
 
-    int a, max = INT_MIN, min = INT_MAX;
+    int a;
+    int max = INT_MIN;
+    // int min = INT_MAX;
     for (int i = 1; i <= n; i++)
     {
         scanf("%d", &a);
