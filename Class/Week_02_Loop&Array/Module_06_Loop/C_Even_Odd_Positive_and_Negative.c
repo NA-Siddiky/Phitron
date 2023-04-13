41c086a9a59c593722d4c8f517ba65d2f7f552e4
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-13 14:11:37 +0600

    problem 3 solved

--- Class/Week_02_Loop&Array/Module_06_Loop/C_Even_Odd_Positive_and_Negative.c
+++ Class/Week_02_Loop&Array/Module_06_Loop/C_Even_Odd_Positive_and_Negative.c
@@ -3,5 +3,31 @@
 int main()
 {
 
+    int x;
+    scanf("%d", &x);
+    int even = 0, odd = 0, pos = 0, neg = 0;
+    int a;
+    for (int i = 1; i <= x; i++)
+    {
+        scanf("%d", &a);
+        if (a % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        if (a > 0)
+        {
+            pos++;
+        }
+        else if (a < 0)
+        {
+            neg++;
+        }
+    }
+    printf("Even: %d\nOdd: %d\nPositive: %d\nNegative: %d\n", even, odd, pos, neg);
+
     return 0;
 }
