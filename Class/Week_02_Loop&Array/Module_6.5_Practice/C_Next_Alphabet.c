3cdb8dc1253c87a68b392e950f6d0f5c8a5730c4
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-13 15:26:59 +0600

    done practice 1

---
+++ Class/Week_02_Loop&Array/Module_6.5_Practice/C_Next_Alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char x;
+    scanf("%c", &x);
+    if (x == 'z')
+    {
+        printf("a");
+    }
+    else
+    {
+        printf("%c", x + 1);
+    }
+    return 0;
+}
