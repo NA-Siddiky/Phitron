77aba33196631110fa2ba5126b3153622f7268c6
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-27 02:13:03 +0600

    done practice problem 1

--- Class/Week_03_Array&String_Operation/Module_11.5_Practice_Day_02/C_Compare.c
+++ Class/Week_03_Array&String_Operation/Module_11.5_Practice_Day_02/C_Compare.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
+    char a[100], b[100];
+    scanf("%s\n", a);
+    scanf("%s", b);
+
+    int value = strcmp(a, b);
+    // printf("%d", value);
+    if (value > 0)
+    {
+        printf("%s", b);
+    }
+    else if (value < 0)
+    {
+        printf("%s\n", a);
+    }
+    // else{
+
+    // }
 
     return 0;
 }
