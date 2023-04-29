e780982dc5ec34df52da17662ac4438789da89b8
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-30 01:14:28 +0600

    done example 13-1

--- Class/Week_04_Function_Pointer/Module_13_Nested_Loop_Recap/13-1_Pattern1.c
+++ Class/Week_04_Function_Pointer/Module_13_Nested_Loop_Recap/13-1_Pattern1.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    int n, k = 1;
+    scanf("%d", &n);
 
+    for (int i = 0; i < n; i++)
+    {
+        // printf("*");
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        k++;
+        printf("\n");
+    }
     return 0;
 }
