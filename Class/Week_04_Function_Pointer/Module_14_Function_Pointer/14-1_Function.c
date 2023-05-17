eedf2029e538f39611c71494619a929975c58426
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-18 01:06:34 +0600

    adding practice problem 14-2 (sum)

--- Class/Week_04_Function_Pointer/Module_14_Function_Pointer/14-1_Function.c
+++ Class/Week_04_Function_Pointer/Module_14_Function_Pointer/14-1_Function.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int sum(int x, int y)
+{
+    int sum = x + y;
+    return sum;
+}
+
 int main()
 {
-
+    int output = sum(5, 10);
+    printf("%d", output);
     return 0;
 }
