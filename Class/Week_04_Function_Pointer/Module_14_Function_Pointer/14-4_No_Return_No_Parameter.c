445cad81462fdca226357e01ccb6051d16ff17f9
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-18 12:15:43 +0600

    done practice problem 14-5

---
+++ Class/Week_04_Function_Pointer/Module_14_Function_Pointer/14-4_No_Return_No_Parameter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void sum(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;
+    printf("%d", sum);
+}
+
+int main()
+{
+
+    sum();
+    return 0;
+}
