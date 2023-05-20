b18da0292addda8cdeb6690588caff7d01c6c67e
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-21 01:40:05 +0600

    done module practice 1-2

---
+++ Semester_1/C++_DSA/Week_01_Introduction/Module_01_Basic/1-2_Print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World";
+    cout << "Hello" << endl
+         << "World\n";
+
+    int a = 100, b = 200;
+    cout << a << " " << b << endl;
+
+    cout << "Ok";
+    return 0;
+}
--- Semester_1/C++_DSA/Week_01_Introduction/Module_01_Basic/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Test C");
-    return 0;
-}
