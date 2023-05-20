c73b2969537cc640f0fcac6420ef742d991f3a27
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-21 02:19:04 +0600

    done module practice 1-5

---
+++ Semester_1/C++_DSA/Week_01_Introduction/Module_01_Basic/1-5_Switch_Case.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int v;
+    // cin >> v;
+    // switch (v)
+    // {
+    // case 1:
+    //     cout << "One" << endl;
+    //     break;
+    // case 2:
+    //     cout << "Two" << endl;
+    //     break;
+    // case 3:
+    //     cout << "Three" << endl;
+    //     break;
+    // case 4:
+    //     cout << "Four" << endl;
+    //     break;
+    // case 5:
+    //     cout << "Five" << endl;
+    //     break;
+    // default:
+    //     cout << "Didn't maatch" << endl;
+    // }
+
+    char a;
+    cin >> a;
+    switch (a)
+    {
+    case 'a':
+        cout << "Vowel" << endl;
+        break;
+    case 'e':
+        cout << "Vowel" << endl;
+        break;
+    case 'i':
+        cout << "Vowel" << endl;
+        break;
+    case 'o':
+        cout << "Vowel" << endl;
+        break;
+    case 'u':
+        cout << "Vowel" << endl;
+        break;
+    default:
+        cout << "Consonant" << endl;
+    }
+
+    return 0;
+}
