8f088493cd751be30259dcfe5df5bc262914f790
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-23 10:21:10 +0600

    check code from win

--- Semester_1/02_C++_DSA/Week_01_Introduction/Module_02_Dynamic_Memory/2-1_Stack_Heap.cpp
+++ Semester_1/02_C++_DSA/Week_01_Introduction/Module_02_Dynamic_Memory/2-1_Stack_Heap.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int a, b;
     cin >> a >> b;
+    swap(a, b);
     cout << a << " " << b << endl;
+
     return 0;
 }
