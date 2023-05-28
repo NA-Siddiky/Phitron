bc849e99e080b923a35705415eb9cb54abbe756e
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-29 00:23:03 +0600

    done excersize 5-7

---
+++ Semester_1/02_C++_DSA/Week_02_Class_and_String/Module_05_String_Class/5-7_String_Input_with_Spaces.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    // cin.ignore();
+    getchar();
+    cout << x << endl;
+
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    cout << s.size() << endl;
+    return 0;
+}
