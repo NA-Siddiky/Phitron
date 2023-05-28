111df02116b7a28554af01c79946e63e072f5c7d
NA-Siddiky <siddiky.academic@gmail.com>
2023-05-29 00:52:17 +0600

    done excersize 6-1

---
+++ Semester_1/02_C++_DSA/Week_02_Class_and_String/Module_06_More_Class_and_String/6-1_String_Constructor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s("Hello World");
+    // string s("Hello World", 5);
+
+    // string a = "Hello World";
+    // string s(a, 7);
+    // cout << s << endl;
+
+    string s(26, 'A');
+    cout << s << endl;
+
+    return 0;
+}
