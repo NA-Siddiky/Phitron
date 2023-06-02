2ad41e0297731cc2a8340fb4d4aac6cf7b6535cf
NA-Siddiky <siddiky.academic@gmail.com>
2023-06-02 15:21:26 +0600

    done assignment problem 2

---
+++ Semester_1/02_C++_DSA/Week_02_Class_and_String/Module_08_Final_Exam_CPP/02_Find.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <sstream> // Added include directive for <sstream>
+
+using namespace std;
+
+class StringInput
+{
+private:
+    string input;
+
+public:
+    StringInput()
+    {
+        getline(cin, input);
+    }
+
+    string getInput()
+    {
+        return input;
+    }
+};
+
+bool finding(const string &input)
+{
+    string word = "";
+    stringstream ss(input);
+    string token;
+    while (ss >> token)
+    {
+        if (token == "Ratul")
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    StringInput strInput;
+    string input = strInput.getInput();
+
+    input += " ";
+
+    bool found = finding(input);
+
+    if (found)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
