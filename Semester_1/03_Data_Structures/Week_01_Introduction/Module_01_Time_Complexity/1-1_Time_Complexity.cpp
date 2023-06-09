8aec66c965ef9e264495cb1c4869cbdb5ce548dd
NA-Siddiky <siddiky.academic@gmail.com>
2023-06-09 19:20:10 +0600

    done excersize 1

--- Semester_1/03_Data_Structures/Week_01_Introduction/Module_01_Time_Complexity/1-1_Time_Complexity.cpp
+++ Semester_1/03_Data_Structures/Week_01_Introduction/Module_01_Time_Complexity/1-1_Time_Complexity.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout << "hello world";
+    int n;
+    cin >> n;
+    // while(n>0) // O(logN)
+    // {
+    //     int digit=n%10;
+    //     cout<<digit<<endl;
+    //     n/=10;
+    // }
+
+    for (int i = 1; i <= n; i = i * 2) // O(logN)
+    {
+        cout << i << endl;
+    }
     return 0;
 }
